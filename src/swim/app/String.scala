91be8f9185d4d41b75385f526a6a1abedfa4b05a
kkrawiec <krzysztof.krawiec@cs.put.poznan.pl>
2017-01-08 07:44:02 +0100

    Char predicates

--- src/swim/app/String.scala
+++ src/swim/app/String.scala
@@ -11,6 +11,7 @@ import swim.tree.Op
 import swim.tree.SimpleGP
 
 case object StringDomain extends Domain[String, String, Op] {
+  type CharPred = Char => Boolean
   override def semantics(input: String) = {
     val l = input.size
     new Function1[Op, Any] {
@@ -18,16 +19,23 @@ case object StringDomain extends Domain[String, String, Op] {
         // Needs toList (otherwise ArrayBuffer, which doesn't work with pattern matching)
         val childRes = op.args.toSeq.map(c => apply(c)).toList
         childRes.+:(op.op) match {
-          case Seq('head, s: String)          => if (s.size > 0) s.substring(0, 1) else ""
-          case Seq('tail, s: String)          => if (s.size > 0) s.substring(1) else ""
-          case Seq('++, s: String, r: String) => s ++ r
-          case Seq('size, s: String)          => s.size
-          case Seq('char, pos: Int)           => if (input.size > 0) input.charAt(pos % l).toString else ""
-          case Seq('input)                    => input
-          case Seq(v: String)                 => v
-          case Seq('+, x: Int, y: Int)        => x + y
-          case Seq(v: Int)                    => v
-          case _                              => throw new Exception("Invalid instruction or arguments: " + op.op + " " + childRes)
+          case Seq('head, s: String)                   => if (s.size > 0) s.substring(0, 1) else ""
+          case Seq('tail, s: String)                   => if (s.size > 0) s.substring(1) else ""
+          case Seq('++, s: String, r: String)          => s ++ r
+          case Seq('size, s: String)                   => s.size
+          case Seq('char, pos: Int)                    => if (input.size > 0) input.charAt(pos % l).toString else ""
+          case Seq('takeWhile, s: String, p: CharPred) => s.takeWhile { p }
+          case Seq('isDigit)                           => ((c: Char) => c.isDigit)
+          case Seq('isAlpha)                           => ((c: Char) => c.isLetter)
+          case Seq('isSpace)                           => ((c: Char) => c == ' ')
+          case Seq('notPred, p: CharPred)              => p.andThen { !_ }
+          case Seq('andPred, p: CharPred, r: CharPred) => ((c: Char) => p(c) && r(c))
+          case Seq('orPred, p: CharPred, r: CharPred)  => ((c: Char) => p(c) || r(c))
+          case Seq('input)                             => input
+          case Seq(v: String)                          => v
+          case Seq('+, x: Int, y: Int)                 => x + y
+          case Seq(v: Int)                             => v
+          case _                                       => throw new Exception("Invalid instruction or arguments: " + op.op + " " + childRes)
         }
       }
     }
@@ -42,6 +50,7 @@ object StringDomainTest extends IApp('maxGenerations -> 100) { // 'parEval -> fa
       'head -> 'str,
       'tail -> 'str,
       '++ -> ('str, 'str),
+      'takeWhile -> ('str, 'charPred),
       'char -> 'int, // a single character in the input string
       'input, // input string
       "some constant string",
@@ -50,7 +59,12 @@ object StringDomainTest extends IApp('maxGenerations -> 100) { // 'parEval -> fa
       'size -> 'str,
       '+ -> ('int, 'int),
       0, 1, 2 // integer constants
-      ))
+      ),
+    'charPred -> Seq(
+      'isDigit, 'isAlpha, 'isSpace,
+      'notPred -> 'charPred,
+      'andPred -> ('charPred, 'charPred),
+      'orPred -> ('charPred, 'charPred)))
 
   { // Task 1: prepend the string with its second character
     val tests = Seq(
@@ -70,5 +84,14 @@ object StringDomainTest extends IApp('maxGenerations -> 100) { // 'parEval -> fa
       Test("fox", "xof"))
     RunExperiment(SimpleGP.Discrete(grammar, StringDomain, tests))
   }
-}
+  { // Task 3: return substring ending before the first space
+    val tests = Seq(
+      Test("", ""),
+      Test("a ", "a"),
+      Test("ab baa baa", "ab"),
+      Test("abc ", "abc"),
+      Test("acka macka", "acka"),
+      Test(" fox", ""))
+    RunExperiment(SimpleGP.Discrete(grammar, StringDomain, tests))
+  }}
 
